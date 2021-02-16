f556ed9eb1690915e06e145a26fc41fa7347d391
thermoweb <romain.chauveau+github@protonmail.com>
2021-02-16 15:09:12 +0100

    test creation

---
+++ src/test/java/org/thermoweb/kata/FizzBuzzTest.java
@@ -0,0 +1,16 @@
+package org.thermoweb.kata;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+class FizzBuzzTest {
+
+    @Test
+    void testFizzBuzz() {
+        assertEquals("Fizz", FizzBuzz.fizzbuzz(3));
+        assertEquals("Buzz", FizzBuzz.fizzbuzz(5));
+        assertEquals("FizzBuzz", FizzBuzz.fizzbuzz(15));
+    }
+
+}
